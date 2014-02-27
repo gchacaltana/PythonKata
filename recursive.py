3f2b00cdfa2662163a3105b76d5ecaa9da995e55
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2014-02-27 12:28:18 -0500

    Update recursive.py

--- recursive.py
+++ recursive.py
@@ -1,13 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Programa que devuelve cadena invertida en secciones de cabeza y cola.
-# @author: Gonzalo Chacaltana B <gchacaltanab@gmail.com>
-def invertirCadena(cadena):
+# @author: Gonzalo Chacaltana Buleje <gchacaltanab@outlook.com>
+
+#importamos el paquete sys para obtener los valores argv enviados por consola.
+import sys
+
+def recursive_reverse(param):
     """ Funcion para invertir cadena en cabeza y cola
     """
-    cabeza = cadena[0:1]
-    cola = cadena[1:len(cadena)]
+    cabeza = param[0:1]
+    cola = param[1:len(param)]
     return cola + cabeza
 
 
-cadena = raw_input("Ingresa una cadena: ")
-print invertirCadena(cadena)
+# main del programa
+if __name__ == "__main__":
+    #almacenamos el valor enviado como parametro en la consola.
+#    param = str(sys.argv[1]) if (sys.argv[1]) else " "
+    if len(sys.argv) > 1:
+        param = sys.argv[1]
+    else:
+        param = " "
+
+    print recursive_reverse(param)
+
 
